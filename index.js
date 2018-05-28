5ef5a508de394cccdff9e255dc27d03e36e3d9d1
Noah Schwarz <noah.anabiik.schwarz@gmail.com>
2018-05-28 18:36:40 +0200

    clean

--- index.js
+++ index.js
@@ -4,42 +4,58 @@ const { parse } = require('url')
 const dealias = require('aka-opts')
 const debug = require('debug')('ads-urls')
 
-const NAV_CONF = { waitUntil: 'networkidle0', timeout: 2000 }
+const NAV_CONF = { waitUntil: 'networkidle0', timeout: 1000 }
 
 async function adsurls (keywords, opts) {
   opts = dealias(opts || {}, { onlyHost: [ 'host', 'strip', 'clean' ] })
   opts = Object.assign({ onlyHost: true }, opts)
+
   const browser = await launch()
   const urlMap = {}
+
   async function scan (browser, keyword) {
     const page = await browser.newPage()
+
+    // go to google and enter the keyword ...
     await page.goto('https://google.com', NAV_CONF)
+    debug('url after 1st nav::', await page.url())
     await page.type('#lst-ib', keyword)
+
     // wait for renavigation ...
     const renav = page.waitForNavigation(NAV_CONF)
     await page.keyboard.press('Enter')
     await renav
-    debug('url after renav::', await page.url())
+    debug('url after 2nd nav::', await page.url())
+
     // select all links with class "plantl pla-hc-c" ...
     // either map hrefs to host domain address or leave link as it is ...
     // uniquify ...
     // store the set of urls in the closed over urlMap ...
-    urlMap[keyword] = await page.$$eval('.plantl.pla-hc-c', links => {
+    const adlinks = await page.$$eval('.plantl.pla-hc-c', links => {
       return [ ...new Set(links.map(link => {
         return opts.onlyHost ? parse(link.href).host : link.href
       })) ]
     })
-    debug('mapped links::', urlMap[keyword])
+
+    debug('mapped links::', adlinks)
+    urlMap[keyword] = adlinks
+
     await page.close()
-    return null
+    return adlinks
   }
+
   return new Promise((resolve, reject) => {
     each(keywords, scan.bind(null, browser), async err => {
       await browser.close()
       err ? reject(err) : resolve(urlMap)
     })
   })
-  // var p = Promise.all()
+  // return new Promise((resolve, reject) => {
+  //   Promise.all(keywords.map(keyword => scan.call(null, browser, keyword)))
+  //     .finally(_ => browser.close())
+  //     .then(_ => resolve(urlMap))
+  //     .catch(reject)
+  // })
 }
 
 module.exports = adsurls
